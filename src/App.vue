58ad2beac9bfb4ebf864062766b1347b592e2991
홍준엽(Ethan) <hjy@gabia.com>
2021-05-24 18:05:23 +0900

    add: Header template 추가

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,4 @@ export default {
 <style lang="scss">
 @import './assets/scss/_global.scss';
 @import './assets/scss/_reset.scss';
-@import './assets/scss/_colors.scss';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 </style>
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,13 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">기간 별 탐색</router-link>
-    |
-    <router-link to="/date">날짜 별 비교</router-link>
-  </nav>
+  <header class="header">
+    <div class="header--content">
+      <span class="header--logo">Dashboard</span>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/_colors.scss';
+@import '@/assets/scss/_mixins.scss';
+
+.header {
+  box-shadow: 0 4px 4px #ddd;
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  &--content {
+    width: 100%;
+    @include content-max-width;
+    padding: 1.5rem 0px;
+  }
+  &--logo {
+    font-size: 2.25rem;
+    font-weight: 700;
+    color: $main-color-1;
+  }
+}
+</style>
