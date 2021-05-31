3f884d60a0a9e5f0089611d79a1f4a413d6cfabf
홍준엽(Ethan) <hjy@gabia.com>
2021-05-31 15:15:29 +0900

    update: LineGraph 컴포넌트 적용

--- src/components/presentationals/period/PeriodLineGraph.vue
+++ src/components/presentationals/period/PeriodLineGraph.vue
@@ -1,44 +1,12 @@
 <template>
-  <section class="period-linegraph">
-    <div class="period-linegraph__label period-linegraph__y-label">동시 접속자 수</div>
-    <div class="period-linegraph__right">
-      <ContentBox>period linegraph</ContentBox>
-      <div class="period-linegraph__label period-linegraph__x-label">날짜</div>
-    </div>
-  </section>
+  <LineGraph xLabel="날짜" yLabel="동시 접속자 수"> line graph </LineGraph>
 </template>
 
 <script>
-import ContentBox from '@/components/presentationals/ContentBox.vue';
+import LineGraph from '@/components/presentationals/LineGraph.vue';
 
 export default {
-  components: { ContentBox },
+  components: { LineGraph },
 };
 </script>
-<style lang="scss" scoped>
-@use '@/assets/styles/base/_colors.scss' as color;
-@use '@/assets/styles/base/_fonts.scss' as font;
-
-.period-linegraph {
-  display: flex;
-  min-width: 1000px;
-  &__right {
-    flex: 1;
-  }
-  &__label {
-    font-size: font.$small;
-    font-weight: font.$bold;
-    color: color.$black-3;
-  }
-  &__y-label {
-    writing-mode: vertical-lr;
-    text-orientation: sideways;
-    transform: rotate(180deg);
-    margin-right: 8px;
-  }
-  &__x-label {
-    text-align: center;
-    margin-top: 8px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
