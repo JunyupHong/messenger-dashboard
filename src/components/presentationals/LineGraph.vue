799857f2f50c9fc8c2158120a7bcc7dee6db7808
홍준엽(Ethan) <hjy@gabia.com>
2021-05-31 15:01:12 +0900

    add: LineGraph 컴포넌트 분리

---
+++ src/components/presentationals/LineGraph.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="linegraph">
+    <div v-if="yLabel" class="linegraph__label linegraph__y-label">{{ yLabel }}</div>
+    <div class="linegraph__right">
+      <ContentBox>
+        <slot></slot>
+      </ContentBox>
+      <div v-if="xLabel" class="linegraph__label linegraph__x-label">{{ xLabel }}</div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ContentBox from '@/components/presentationals/ContentBox.vue';
+
+export default {
+  props: {
+    yLabel: String,
+    xLabel: String,
+  },
+  components: { ContentBox },
+};
+</script>
+<style lang="scss" scoped>
+@use '@/assets/styles/base/_colors.scss' as color;
+@use '@/assets/styles/base/_fonts.scss' as font;
+
+.linegraph {
+  display: flex;
+  min-width: 1000px;
+  &__right {
+    flex: 1;
+  }
+  &__label {
+    font-size: font.$small;
+    font-weight: font.$bold;
+    color: color.$black-3;
+  }
+  &__y-label {
+    writing-mode: vertical-lr;
+    text-orientation: sideways;
+    transform: rotate(180deg);
+    margin-right: 8px;
+  }
+  &__x-label {
+    text-align: center;
+    margin-top: 8px;
+  }
+}
+</style>
