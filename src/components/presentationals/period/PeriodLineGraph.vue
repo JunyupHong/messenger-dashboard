ded3a8dfa91cbf3a8d9484a53f7e8bab7ab0a2df
홍준엽(Ethan) <hjy@gabia.com>
2021-05-31 14:49:05 +0900

    add: PeriodLineGraph 템플릿 개발

---
+++ src/components/presentationals/period/PeriodLineGraph.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="period-linegraph">
+    <div class="period-linegraph__label period-linegraph__y-label">동시 접속자 수</div>
+    <div class="period-linegraph__right">
+      <ContentBox>period linegraph</ContentBox>
+      <div class="period-linegraph__label period-linegraph__x-label">날짜</div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ContentBox from '@/components/presentationals/ContentBox.vue';
+
+export default {
+  components: { ContentBox },
+};
+</script>
+<style lang="scss" scoped>
+@use '@/assets/styles/base/_colors.scss' as color;
+@use '@/assets/styles/base/_fonts.scss' as font;
+
+.period-linegraph {
+  display: flex;
+  min-width: 1000px;
+  &__right {
+    flex: 1;
+  }
+  &__label {
+    font-size: font.$small;
+    font-weight: font.$bold;
+    color: color.$black-3;
+  }
+  &__y-label {
+    writing-mode: vertical-lr;
+    text-orientation: sideways;
+    transform: rotate(180deg);
+    margin-right: 8px;
+  }
+  &__x-label {
+    text-align: center;
+    margin-top: 8px;
+  }
+}
+</style>
