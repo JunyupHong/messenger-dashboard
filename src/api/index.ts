7bf591f71f6c7adf694bee9c9fac588a89d5f8c3
홍준엽(Ethan) <hjy@gabia.com>
2021-06-21 10:58:10 +0900

    update: api migration

---
+++ src/api/index.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const baseURL = 'https://dev-msgr-statistics-api.hiworks.com/v1/statistics';
+const getDateQuery = (date: string): string => `/today/${date}`;
+const getPeriodQuery = (startDate: string, endDate: string) =>
+  `/term/start-date/${startDate}/end-date/${endDate}`;
+
+export const fetchDate = async date => {
+  try {
+    const response = await axios.get(baseURL + getDateQuery(date));
+    return response.data.data;
+  } catch (e) {
+    throw new Error(` ${e.message}\nFetch Date : ${baseURL + getDateQuery(date)}`);
+  }
+};
+
+export const fetchPeriod = async (startDate, endDate) => {
+  try {
+    const response = await axios.get(baseURL + getPeriodQuery(startDate, endDate));
+    return response.data.data;
+  } catch (e) {
+    throw new Error(`${e.message}\nFetch Period : ${baseURL + getPeriodQuery(startDate, endDate)}`);
+  }
+};
+
+export default {
+  fetchDate,
+  fetchPeriod,
+};
