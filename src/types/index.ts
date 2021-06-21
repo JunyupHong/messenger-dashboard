53c698c169ce7b202ea6f88e8d09039236719d56
홍준엽(Ethan) <hjy@gabia.com>
2021-06-21 15:43:00 +0900

    add: type 추가

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Legend_T {
+  name: string;
+  active: boolean;
+}
+
+export interface Server_T {
+  conn_date: string;
+  conn_hours: number;
+  id: string;
+  max_user: number;
+  server_ip: string;
+  serverinfo_uid: string;
+}
